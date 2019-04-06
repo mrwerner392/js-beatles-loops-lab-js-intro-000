38fa7e883297626a81be485e9b6abf847c70e833
Matt Werner <mrwerner392@gmail.com>
2019-04-06 18:07:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// add solution here
+// loop through two arrays to make one array with a for loop
+function theBeatlesPlay(musicians, instruments) {
+  let theBeatles = [];
+
+  for (let i = 0; i < musicians.length; i++) {
+    theBeatles[i] = `${musicians[i]} plays ${instruments[i]}`;
+  }
+
+  return theBeatles;
+}
+
+// add '!!!' to strings in an array with a while loop
+function johnLennonFacts(factsArray) {
+  let i = 0;
+
+  while (i < factsArray.length) {
+    factsArray[i] += '!!!';
+  }
+
+  return factsArray;
+}
